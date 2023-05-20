699b6db437a872546353e5386dbc24f32a3a92f9
Jerry Nixon <jnixon@microsoft.com>
2023-05-19 23:25:24 -0600

    Updatred Models

--- DabHelper.Harness/DabModels.g.cs
+++ DabHelper.Harness/DabModels.g.cs
@@ -1,5 +1,4 @@
-﻿
-// more info at https://aka.ms/dab
+﻿// more info at https://aka.ms/dab
 
 using System;
 using System.Diagnostics;
@@ -15,22 +14,25 @@ namespace Models
 
 namespace Models.dbo
 {
-    [DebuggerDisplay("dbo.PageCustomers (StartIndex = {Parameters.StartIndex}, PageSize = {Parameters.PageSize}) [procedure]")]
+    [DebuggerDisplay("dbo.PageCustomers (PageSize = {Parameters.PageSize}, StartIndex = {Parameters.StartIndex}) [procedure]")]
     public class @PageCustomers : Poco 
     {
-        [JsonPropertyName("city")]
-        public string @City { get; set; } = default!;
-
+        [ReadOnly(true)]
         [JsonPropertyName("id")]
         public int @Id { get; set; } = default!;
 
+        [ReadOnly(true)]
         [JsonPropertyName("name")]
         public string @Name { get; set; } = default!;
 
+        [ReadOnly(true)]
+        [JsonPropertyName("city")]
+        public string @City { get; set; } = default!;
+
+        [ReadOnly(true)]
         [JsonPropertyName("state")]
         public string @State { get; set; } = default!;
 
-    
         public ParameterInfo Parameters { get; set; } = new();
         public class ParameterInfo
         {
@@ -39,52 +41,22 @@ namespace Models.dbo
         }
     }
 
-    [DebuggerDisplay("dbo.Top10Customers (PageSize = {Parameters.PageSize}, StartIndex = {Parameters.StartIndex}) [procedure]")]
+    [DebuggerDisplay("dbo.Top10Customers ( = {Parameters.}) [procedure]")]
     public class @Top10Customers : Poco 
     {
-        [JsonPropertyName("city")]
-        public string @City { get; set; } = default!;
-
+        [ReadOnly(true)]
         [JsonPropertyName("id")]
         public int @Id { get; set; } = default!;
 
-        [JsonPropertyName("location")]
-        public string @Location { get; set; } = default!;
-
+        [ReadOnly(true)]
         [JsonPropertyName("name")]
         public string @Name { get; set; } = default!;
 
-        [JsonPropertyName("special-rank")]
-        public decimal @SpecialRank { get; set; } = default!;
-
-        [JsonPropertyName("state")]
-        public string @State { get; set; } = default!;
-
-    
-        public ParameterInfo Parameters { get; set; } = new();
-        public class ParameterInfo
-        {
-            public int? @PageSize { get; set; } = default!;
-            public int? @StartIndex { get; set; } = default!;
-        }
-    }
-
-    [DebuggerDisplay("dbo.Customers (Id = {Id}) [table]")]
-    public class @Customers : Poco 
-    {
-        [Key]
-        [JsonPropertyName("id")]
-        public int @Id { get; set; } = default!;
-
+        [ReadOnly(true)]
         [JsonPropertyName("city")]
         public string @City { get; set; } = default!;
 
-        [JsonPropertyName("name")]
-        public string @Name { get; set; } = default!;
-
-        [JsonPropertyName("special-rank")]
-        public decimal @SpecialRank { get; set; } = default!;
-
+        [ReadOnly(true)]
         [JsonPropertyName("state")]
         public string @State { get; set; } = default!;
 
@@ -92,14 +64,45 @@ namespace Models.dbo
         [JsonPropertyName("location")]
         public string @Location { get; set; } = default!;
 
+        [ReadOnly(true)]
+        [JsonPropertyName("special-rank")]
+        public decimal @SpecialRank { get; set; } = default!;
+
     }
 
-    [DebuggerDisplay("dbo.Lines (Id = {Id}) [table]")]
-    public class @Lines : Poco 
+    [DebuggerDisplay("dbo.Customers (Id = {Id},  = {Parameters.}) [table]")]
+    public class @Customers : Poco 
     {
+        [JsonPropertyName("name")]
+        public string @Name { get; set; } = default!;
+
+        [JsonPropertyName("city")]
+        public string @City { get; set; } = default!;
+
+        [JsonPropertyName("state")]
+        public string @State { get; set; } = default!;
+
+        [JsonPropertyName("special-rank")]
+        public decimal @SpecialRank { get; set; } = default!;
+
+        [ReadOnly(true)]
+        [JsonPropertyName("location")]
+        public string @Location { get; set; } = default!;
+
         [Key]
         [JsonPropertyName("id")]
         public int @Id { get; set; } = default!;
+
+    }
+
+    [DebuggerDisplay("dbo.Lines (Id = {Id},  = {Parameters.}) [table]")]
+    public class @Lines : Poco 
+    {
+        [JsonPropertyName("quantity")]
+        public int @Quantity { get; set; } = default!;
+
+        [JsonPropertyName("product-id")]
+        public int @ProductId { get; set; } = default!;
 
         [JsonPropertyName("order-id")]
         public int @OrderId { get; set; } = default!;
@@ -107,47 +110,64 @@ namespace Models.dbo
         [JsonPropertyName("price-each")]
         public double @PriceEach { get; set; } = default!;
 
-        [JsonPropertyName("product-id")]
-        public int @ProductId { get; set; } = default!;
-
-        [JsonPropertyName("quantity")]
-        public int @Quantity { get; set; } = default!;
-
-    }
-
-    [DebuggerDisplay("dbo.Orders (Id = {Id}) [table]")]
-    public class @Orders : Poco 
-    {
         [Key]
         [JsonPropertyName("id")]
         public int @Id { get; set; } = default!;
+
+    }
+
+    [DebuggerDisplay("dbo.Orders (Id = {Id},  = {Parameters.}) [table]")]
+    public class @Orders : Poco 
+    {
+        [JsonPropertyName("date")]
+        public DateTime? @Date { get; set; } = default!;
 
         [JsonPropertyName("customer-id")]
         public int @CustomerId { get; set; } = default!;
 
-        [JsonPropertyName("date")]
-        public DateTime? @Date { get; set; } = default!;
-
-    }
-
-    [DebuggerDisplay("dbo.Products (Id = {Id}) [table]")]
-    public class @Products : Poco 
-    {
         [Key]
         [JsonPropertyName("id")]
         public int @Id { get; set; } = default!;
 
+    }
+
+    [DebuggerDisplay("dbo.Products (Id = {Id},  = {Parameters.}) [table]")]
+    public class @Products : Poco 
+    {
         [JsonPropertyName("name")]
         public string @Name { get; set; } = default!;
 
         [JsonPropertyName("price")]
         public double @Price { get; set; } = default!;
 
+        [Key]
+        [JsonPropertyName("id")]
+        public int @Id { get; set; } = default!;
+
     }
 
-    [DebuggerDisplay("dbo.Sample (Id1 = {Id1}, Id2 = {Id2}, Id3 = {Id3}) [table]")]
+    [DebuggerDisplay("dbo.Sample (Id1 = {Id1}, Id2 = {Id2}, Id3 = {Id3},  = {Parameters.}) [table]")]
     public class @Sample : Poco 
     {
+        [JsonPropertyName("name")]
+        public string @Name { get; set; } = default!;
+
+        [JsonPropertyName("weird-name")]
+        public string @WeirdName { get; set; } = default!;
+
+        [JsonPropertyName("super-weird-name-here")]
+        public string @SuperWeirdNameHere { get; set; } = default!;
+
+        [JsonPropertyName("very-strange-like-name")]
+        public string @VeryStrangeLikeName { get; set; } = default!;
+
+        [JsonPropertyName("lowercase")]
+        public int? @Lowercase { get; set; } = default!;
+
+        [ReadOnly(true)]
+        [JsonPropertyName("upper-name")]
+        public string @UpperName { get; set; } = default!;
+
         [Key]
         [JsonPropertyName("id1")]
         public int @Id1 { get; set; } = default!;
@@ -160,28 +180,9 @@ namespace Models.dbo
         [JsonPropertyName("id3")]
         public int @Id3 { get; set; } = default!;
 
-        [JsonPropertyName("lowercase")]
-        public int? @Lowercase { get; set; } = default!;
-
-        [JsonPropertyName("name")]
-        public string @Name { get; set; } = default!;
-
-        [JsonPropertyName("super-weird-name-here")]
-        public string @SuperWeirdNameHere { get; set; } = default!;
-
-        [JsonPropertyName("very-strange-like-name")]
-        public string @VeryStrangeLikeName { get; set; } = default!;
-
-        [JsonPropertyName("weird-name")]
-        public string @WeirdName { get; set; } = default!;
-
-        [ReadOnly(true)]
-        [JsonPropertyName("upper-name")]
-        public string @UpperName { get; set; } = default!;
-
     }
 
-    [DebuggerDisplay("dbo.SampleEdgeTwo (keyless) [table]")]
+    [DebuggerDisplay("dbo.SampleEdgeTwo ( = {Parameters.}) [table]")]
     public class @SampleEdgeTwo : Poco 
     {
         [JsonPropertyName("id")]
@@ -189,7 +190,7 @@ namespace Models.dbo
 
     }
 
-    [DebuggerDisplay("dbo.SampleNode (Id = {Id}) [table]")]
+    [DebuggerDisplay("dbo.SampleNode (Id = {Id},  = {Parameters.}) [table]")]
     public class @SampleNode : Poco 
     {
         [Key]
@@ -198,26 +199,26 @@ namespace Models.dbo
 
     }
 
-    [DebuggerDisplay("dbo.vCustomers (keyless) [view]")]
+    [DebuggerDisplay("dbo.vCustomers ( = {Parameters.}) [view]")]
     public class @VCustomers : Poco 
     {
-        [JsonPropertyName("city")]
-        public string @City { get; set; } = default!;
-
         [JsonPropertyName("id")]
         public int @Id { get; set; } = default!;
-
-        [JsonPropertyName("location")]
-        public string @Location { get; set; } = default!;
 
         [JsonPropertyName("name")]
         public string @Name { get; set; } = default!;
 
-        [JsonPropertyName("special-rank")]
-        public decimal @SpecialRank { get; set; } = default!;
+        [JsonPropertyName("city")]
+        public string @City { get; set; } = default!;
 
         [JsonPropertyName("state")]
         public string @State { get; set; } = default!;
+
+        [JsonPropertyName("location")]
+        public string @Location { get; set; } = default!;
+
+        [JsonPropertyName("special-rank")]
+        public decimal @SpecialRank { get; set; } = default!;
 
         [JsonPropertyName("upper-name")]
         public string @UpperName { get; set; } = default!;
